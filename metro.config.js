be8b7507490048895a5013ad61f851de96f47473
Vivek Kumar <vivek@culturtap.com>
2024-06-04 18:48:04 +0530

    updated

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,43 @@
 
+// const { getDefaultConfig } = require("expo/metro-config");
+// const { withNativeWind } = require('nativewind/metro');
+
+// module.exports = (async () => {
+//     const defaultConfig = getDefaultConfig(__dirname);
+
+//     // Apply NativeWind configuration
+//     const configWithNativeWind = withNativeWind(defaultConfig, { input: './global.css' });
+
+//     // Modify resolver and transformer for SVG support
+//     const { transformer, resolver } = configWithNativeWind;
+
+//     const modifiedTransformer = {
+//         ...transformer,
+//         babelTransformerPath: require.resolve("react-native-svg-transformer")
+//     };
+
+//     const modifiedResolver = {
+//         ...resolver,
+//         assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+//         sourceExts: [...resolver.sourceExts, "svg"]
+//     };
+
+//     // Update the configuration with modified resolver and transformer
+//     const updatedConfig = {
+//         ...configWithNativeWind,
+//         transformer: modifiedTransformer,
+//         resolver: modifiedResolver
+//     };
+
+//     return updatedConfig;
+// })();
+
+
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
 
 module.exports = (async () => {
-    const defaultConfig = getDefaultConfig(__dirname);
+    const defaultConfig = await getDefaultConfig(__dirname);
 
     // Apply NativeWind configuration
     const configWithNativeWind = withNativeWind(defaultConfig, { input: './global.css' });
@@ -19,7 +53,7 @@ module.exports = (async () => {
     const modifiedResolver = {
         ...resolver,
         assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-        sourceExts: [...resolver.sourceExts, "svg"]
+        sourceExts: [...resolver.sourceExts, "svg", "css"]
     };
 
     // Update the configuration with modified resolver and transformer
@@ -31,6 +65,3 @@ module.exports = (async () => {
 
     return updatedConfig;
 })();
-
-
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,78 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   // NOTE: Update this to include the paths to all of your component files.
+//   content: ["./app/**/*.{js,jsx,ts,tsx}"],
+//   presets: [require("nativewind/preset")],
+//   theme: {
+//     extend: {
+//       textColor: {
+//         text: '#2e2c43', //black
+//         primary: '#fb8c00',//dark orange
+//         secondary: '#ffe7c8', //light orange
+//         danger: '#e76063', // red
+//         inputBoxBg: '#f9f9f9', // pure white
+//         successText: '#558b2f', //green
+
+//       },
+
+//       sizes: {
+//         first: '18px',
+//         second: '16px',
+//         third: '14px',
+//         fourth: '13px',
+//         fifth: '12px',
+//         sixth: '11px',
+//       },
+
+//       fontStyle: {
+//         first: 'extrabold',
+//         second: 'bold',
+//         third: 'semiboldl',
+//         fourth: 'light',
+//       },
+
+//       borderRadius: {
+//         first: '3xl',
+//         second: '2xl',
+//         third: 'xl',
+//         fourth: 'lg',
+//         fifth: 'md',
+//         sixth: 'sm',
+//       },
+
+//       background: {
+//         primary: '#fb8c00', //dark orange
+//         secondary: '#ffe7c8', //light orange
+//         success: '#70b241', //green
+//         danger: '#e76063', //red
+//         chatUserBg: '#ebebeb', //light grey
+//         chatRetailerBg: '#fafafa', //pure white
+//         userAccountBg: '#f9f9f9', //extra white
+//         dummyUserBg: '#f9f9f9', // extra white
+//         textInputBg: '#f9f9f9', //extra white
+//         border: '#2e2c43' //black
+//       }
+//     },
+//   },
+//   plugins: [],
+// }
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
       textColor: {
-        text: '#2e2c43', //black
-        primary: '#fb8c00',//dark orange
-        secondary: '#ffe7c8', //light orange
-        danger: '#e76063', // red
-        inputBoxBg: '#f9f9f9', // pure white
-        successText: '#558b2f', //green
-
+        text: '#2e2c43',
+        primary: '#fb8c00',
+        secondary: '#ffe7c8',
+        danger: '#e76063',
+        inputBoxBg: '#f9f9f9',
+        successText: '#558b2f',
       },
-
-      sizes: {
+      fontSize: {
         first: '18px',
         second: '16px',
         third: '14px',
@@ -23,14 +80,12 @@ module.exports = {
         fifth: '12px',
         sixth: '11px',
       },
-
-      fontStyle: {
+      fontWeight: {
         first: 'extrabold',
         second: 'bold',
-        third: 'semiboldl',
+        third: 'semibold',
         fourth: 'light',
       },
-
       borderRadius: {
         first: '3xl',
         second: '2xl',
@@ -39,19 +94,18 @@ module.exports = {
         fifth: 'md',
         sixth: 'sm',
       },
-
-      background: {
-        primary: '#fb8c00', //dark orange
-        secondary: '#ffe7c8', //light orange
-        success: '#70b241', //green
-        danger: '#e76063', //red
-        chatUserBg: '#ebebeb', //light grey
-        chatRetailerBg: '#fafafa', //pure white
-        userAccountBg: '#f9f9f9', //extra white
-        dummyUserBg: '#f9f9f9', // extra white
-        textInputBg: '#f9f9f9', //extra white
-        border: '#2e2c43' //black
-      }
+      backgroundColor: {
+        primary: '#fb8c00',
+        secondary: '#ffe7c8',
+        success: '#70b241',
+        danger: '#e76063',
+        chatUserBg: '#ebebeb',
+        chatRetailerBg: '#fafafa',
+        userAccountBg: '#f9f9f9',
+        dummyUserBg: '#f9f9f9',
+        textInputBg: '#f9f9f9',
+        border: '#2e2c43',
+      },
     },
   },
   plugins: [],
