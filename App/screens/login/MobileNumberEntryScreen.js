077c7700efbc29ddb0cf0a974ea8a40297e37288
chandresh-t75 <2100430100016@bietjhs.ac.in>
2024-06-12 14:50:31 +0530

    "auth changes"

--- App/screens/login/MobileNumberEntryScreen.js
+++ App/screens/login/MobileNumberEntryScreen.js
@@ -131,10 +131,10 @@ const MobileNumberEntryScreen = () => {
       setLoading(true);
       try {
         const phoneNumber = countryCode + mobileNumber;
-        // console.log(phoneNumber);
-        // const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
-        // setConfirm(confirmation);
-        // console.log(confirmation);
+        console.log(phoneNumber);
+        const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+        setConfirm(confirmation);
+        console.log(confirmation);
         dispatch(setMobileNumber(phoneNumber));
         setMobileScreen(false);
       } catch (error) {
@@ -153,11 +153,11 @@ const MobileNumberEntryScreen = () => {
     try {
       // Make a request to your backend API to check if the mobile number is registered
 
-      // console.log(confirm)
-      // const res = await confirm.confirm(otp);
-      // console.log("res", res);
+      console.log(confirm)
+      const res = await confirm.confirm(otp);
+      console.log("res", res);
       console.log(otp);
-      // if(res){
+      if(res){
       const phoneNumber = countryCode + mobileNumber;
       console.log("phone", phoneNumber);
       const response = await axios.get("http://173.212.193.109:5000/user/", {
@@ -201,13 +201,13 @@ const MobileNumberEntryScreen = () => {
         // setMobileNumberLocal("");
         navigation.navigate("registerUsername");
       }
-      //   }
-      //   else{
-      //     setLoading(false);
-      //     console.log('Invalid otp:');
-      //     alert('Invalid otp');
-      //     return;
-      //   }
+        }
+        else{
+          setLoading(false);
+          console.log('Invalid otp:');
+          alert('Invalid otp');
+          return;
+        }
     } catch (error) {
       console.error("Error checking mobile number:", error);
     } finally {
