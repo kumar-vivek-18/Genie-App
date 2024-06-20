34b6a259190bc9c10111f659d0b118152a6e4d8e
Vivek Kumar <vivek@culturtap.com>
2024-06-20 13:12:07 +0530

    updated flow of user app

--- App/screens/createRequest/RequestEntry.js
+++ App/screens/createRequest/RequestEntry.js
@@ -1,12 +1,13 @@
 import { View, Text, Pressable, TextInput, TouchableOpacity } from 'react-native';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import ArrowLeft from '../../assets/arrow-left.svg';
 import Genie from '../../assets/Genie.svg';
 import { useNavigation } from '@react-navigation/native';
 import { setRequestDetail } from '../../redux/reducers/userRequestsSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import BackArrow from "../../assets/BackArrowImg.svg";
+import axios from 'axios';
 
 
 const RequestEntry = () => {
@@ -15,10 +16,31 @@ const RequestEntry = () => {
     const [query, setQuery] = useState("");
     const requestDetail = useSelector(store => store.userRequest.requestDetail);
 
+    const userLongitude = useSelector(store => store.user.userLongitude);
+    const userLatitude = useSelector(store => store.user.userLatitude);
+    const fetchLocation = useCallback(async () => {
+        try {
+            await axios.get('http://192.212.193.109:5000/retailer/stores-near-me', {
+                params: {
+                    longitude: userLongitude,
+                    latitude: userLatitude,
+                }
+            })
+                .then(res => {
+                    setNearByStoresCategory(res.data);
+                })
+        } catch (error) {
+            console.error("error while fetching nearby stores", error);
+        }
+    })
 
+    useEffect(() => {
+
+
+    })
 
     return (
-        <View style={{ flex: 1,backgroundColor: "white"  }}>
+        <View style={{ flex: 1, backgroundColor: "white" }}>
             <View style={{ flex: 1 }}>
 
                 <View className=" w-full flex flex-row px-[29px]  justify-between absolute  top-[60px]">
@@ -31,14 +53,14 @@ const RequestEntry = () => {
                 <View className="flex-row justify-center mt-[40px] mb-[10px] ">
                     <Genie width={35} height={52} />
                 </View>
-                <Text className="text-[14.5px]  text-[#FB8C00] text-center mb-[10px] " style={{fontFamily:"Poppins-Medium"}}>
+                <Text className="text-[14.5px]  text-[#FB8C00] text-center mb-[10px] " style={{ fontFamily: "Poppins-Medium" }}>
                     Step 1/4
                 </Text>
 
                 <View className="px-[32px] mb-[20px]">
-                    <Text className="text-[16px]  text-[#2e2c43] text-center mb-[18px] " style={{fontFamily:"Poppins-Black"}}>Type your spade</Text>
+                    <Text className="text-[16px]  text-[#2e2c43] text-center mb-[18px] " style={{ fontFamily: "Poppins-Black" }}>Type your spade</Text>
 
-                    <Text className="text-[14px] mx-[16px] mt-[15px] text-center text-[#2e2c43]" style={{fontFamily:"Poppins-Regular"}}>like: My phone charger get damage / I want a 55 inch screen tv / I need a plumber to repair my water supply. </Text>
+                    <Text className="text-[14px] mx-[16px] mt-[15px] text-center text-[#2e2c43]" style={{ fontFamily: "Poppins-Regular" }}>like: My phone charger get damage / I want a 55 inch screen tv / I need a plumber to repair my water supply. </Text>
 
                 </View>
 
@@ -53,7 +75,7 @@ const RequestEntry = () => {
                         placeholder="Type here..."
                         placeholderTextColor="#dbcdbb"
                         className="w-full h-[127px] overflow-y-scroll px-[20px] border-[0.3px] border-[#2e2c43] rounded-xl"
-                        style={{ padding: 20, height: 300, flex: 1, textAlignVertical: 'top',fontFamily:"Poppins-Regular" }}
+                        style={{ padding: 20, height: 300, flex: 1, textAlignVertical: 'top', fontFamily: "Poppins-Regular" }}
                     />
                 </View>
 
@@ -85,7 +107,7 @@ const RequestEntry = () => {
                     <Text
                         style={{
                             fontSize: 18,
-                            fontFamily:"Poppins-Black" ,
+                            fontFamily: "Poppins-Black",
                             color: !query ? "#888888" : "white",
                         }}
                     >
