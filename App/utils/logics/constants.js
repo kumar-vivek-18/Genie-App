70725b9bd66c3bbbfc03770f7d8f80d682c43276
Vivek Kumar <vivek@culturtap.com>
2024-07-22 16:47:00 +0530

    updated code

--- App/utils/logics/constants.js
+++ App/utils/logics/constants.js
@@ -1,3 +1,3 @@
-// export const baseUrl = 'http://173.212.193.109:5000';
+export const baseUrl = 'http://173.212.193.109:5000';
 
-export const baseUrl = 'http://192.168.212.192:5000';
+// export const baseUrl = 'http://192.168.212.192:5000';
